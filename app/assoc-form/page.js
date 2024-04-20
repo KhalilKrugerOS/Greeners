8b6bee3a988f00241ee96b2afa53fa2aaaeaac32
Saif <mohamedsaifeddine.kamoun@insat.ucar.tn>
2024-04-20 17:25:06 +0100

    com3

---
+++ app/assoc-form/page.js
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+import AssocForm from "@components/AssocForm";
+
+const CreatePrompt = () => {
+    const { data: session } = useSession();
+    const router = useRouter();
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        title: "",
+        prompt: "",
+        test:"test"
+    });
+    const createPrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+        try {
+            const response = await fetch("/api/prompt/new", {
+                method: "POST",
+                body: JSON.stringify({
+                    mail: post.mail,
+                    userId: session?.user.id,
+                    cname: post.cname,
+                    number: post.number,
+                }),
+            });
+            if (response.ok) {
+                router.push("/");
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setSubmitting(false);
+        }
+    };
+
+    return (
+        <AssocForm
+            type="Add"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
+    );
+};
+
+export default CreatePrompt;
